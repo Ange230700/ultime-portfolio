2cf4597db66e59a1b7bef8f353613d29c46ec8a7
Ange230700 <kouakouangeericstephane@gmail.com>
2025-05-04 20:54:01 +0200

    feat: 🎸  implement about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,8 +4,55 @@
 
 export default function AboutPage() {
   return (
-    <div className="flex flex-1 flex-col p-4">
-      <h1 className="mb-4 text-3xl font-bold">À propos</h1>
+    <div className="mx-auto flex max-w-3xl flex-1 flex-col space-y-6 p-4">
+      <h1 className="text-3xl font-bold">À propos de moi</h1>
+
+      <p className="text-lg leading-relaxed">
+        Bonjour, je suis <strong>Ange KOUAKOU</strong>, actuellement apprenant à
+        l’ADA Tech School. Ma progression dans la programmation a débuté en
+        2016-2017 au lycée où j&apos;apprenais les bases du langage C. J’étais
+        captivé par la possibilité de transformer des concepts mathématiques en
+        réalisations concrètes à l&apos;écran.
+      </p>
+
+      <p className="text-lg leading-relaxed">
+        Très vite, j’ai découvert que la programmation va bien au-delà de
+        l’application de concepts mathématiques : c’est une discipline qui
+        consiste à résoudre des problèmes réels et complexes de manière logique
+        et créative, et à participer activement à la construction du monde
+        numérique.
+      </p>
+
+      <p className="text-lg leading-relaxed">
+        Pour confirmer mon projet professionnel, j’ai suivi un bootcamp de 5
+        mois en développement web à la Wild Code School. Cette expérience m’a
+        conforté dans mon choix de devenir développeur web. Aujourd’hui, à ADA
+        Tech School, j’aiguises mes compétences en technologies frontend et
+        backend, notamment TypeScript, PHP, Java et Python.
+      </p>
+
+      <p className="text-lg leading-relaxed">
+        J’ai travaillé sur plusieurs projets en équipe, comme une application
+        PHP pour gérer et planifier les opérations de recyclage d&apos;une
+        association. Ce projet m’a permis de maîtriser l’authentification et la
+        gestion des droits utilisateur, tout en développant mon goût pour le
+        travail en équipe et les principes de l’agilité.
+      </p>
+
+      <p className="text-lg leading-relaxed">
+        Dans le but d’obtenir la certification RNCP de niveau 6 en tant que
+        concepteur-développeur d’applications, je suis à la recherche d’une
+        alternance de 12 à 36 mois (quatre jours en entreprise et un jour en
+        formation) à partir de juin 2025. Rejoindre votre entreprise serait pour
+        moi l’occasion de mettre en pratique mes compétences fullstack et de
+        contribuer à des projets concrets.
+      </p>
+
+      <p className="text-lg leading-relaxed">
+        Mon objectif ultime est d’atteindre la certification RNCP de niveau 7 en
+        architecture logicielle et de participer à la création de solutions
+        robustes et innovantes.
+      </p>
     </div>
   );
 }
